7420b68f551f1d6cc97b642c7566f90db48e2a22
SurfaceTens <sergio_perea_91@hotmail.com>
2023-06-05 23:00:01 +0200

    Fix en ORMs

---
+++ src/main/java/com/dim/autotestAPI/entidades/PreguntaExamenConID.java
@@ -0,0 +1,17 @@
+package com.dim.autotestAPI.entidades;
+
+import es.mde.acing.utils.PreguntaExamenImpl;
+
+public class PreguntaExamenConID extends PreguntaExamenImpl {
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
